5c9aff2f7c130c18c91641f44db1cde5806e5b2c
William Chenta <william.chenta@gmail.com>
2020-04-20 15:00:42 -0300

    projeto exemplo vuex

---
+++ src/components/Dashoboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+      <div class="veiculo" v-for="veiculo in veiculos" :key="veiculo.placa" :style="veiculo.cor">
+          {{veiculo.placa}}
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        veiculos() {
+            return this.$store.state.veiculos
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .container {
+        width: 400px;
+        height: 300px;
+        border: 1px solid #000;
+        display: flex;
+        flex-wrap: wrap;
+        padding: 20px;
+    }
+    .veiculo {
+        min-width: 50px;
+        height: 50px;
+        border: 1px solid #000;
+        margin-left: 5px;
+    }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    veiculos: [{
+      placa: "mbr5566",
+      cor: "branco"
+    }, {
+      placa: "xdr9988",
+      cor: "azul"
+    }, {
+      placa: "tyu3245",
+      cor: "azul"
+    }]
   },
   mutations: {
+    cadastrarVeiculo(state, veiculo) {
+      state.veiculos.push(veiculo)
+    }
   },
   actions: {
   },
